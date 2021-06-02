8b56d9853acbcfc3fb18c0811057a21fe1ea12eb
Rubem <rfsv@ic.ufal.br>
2021-06-01 22:28:28 -0300

    feature: handling mouse hover pop ups

--- cypress/integration/examples/test6.spec.js
+++ cypress/integration/examples/test6.spec.js
@@ -4,7 +4,7 @@ context('My Sixth Test Suite', () => {
   it('should be my sixth test', () => {
     cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
 
-    // Handling Web Tables With Cypress Usinh Each Command
+    // Handling Mouse Hover Pop ups
     cy.get('div.mouse-hover-content').invoke('show');
     cy.contains('Top').click();
     cy.url().should('include', 'top');
