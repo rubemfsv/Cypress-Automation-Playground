940048f965e22650a41837004643e23ed6706614
Rubem <rfsv@ic.ufal.br>
2021-06-17 18:44:13 -0300

    feature: create tests to handle with iframe

---
+++ cypress/integration/examples/test8.spec.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+/// <reference types="cypress-iframe" />
+import 'cypress-iframe';
+
+context('My Eighth Test Suite', () => {
+  it('should be my eighth test', () => {
+    cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+
+    // Handle iFrame loading frame
+    cy.frameLoaded('#courses-iframe');
+
+    // Handle iFrame by clicking in link inside iFrame
+    cy.iframe().find("a[href*='mentorship']").eq(0).click();
+
+    cy.iframe().find("h1[class*='pricing-title']").should('have.length', 2);
+  });
+});
