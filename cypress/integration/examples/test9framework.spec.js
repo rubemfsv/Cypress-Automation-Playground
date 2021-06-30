47ec9bdf356c5b704d998459761916394d64d64c
Rubem <rfsv@ic.ufal.br>
2021-06-30 20:36:43 -0300

    chore: uses page objects

--- cypress/integration/examples/test9framework.spec.js
+++ cypress/integration/examples/test9framework.spec.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import HomePage from '../pageObjects/HomePage';
+import ProductsPage from '../pageObjects/ProductsPage';
 
 import 'cypress-iframe';
 
@@ -12,30 +14,47 @@ context('My Nineth Test Suite Using Framework', () => {
 
   // Uses of before hooks and fixtures
   it('should be my nineth test', () => {
+    const homePage = new HomePage();
+    const productsPage = new ProductsPage();
+
     cy.visit('https://rahulshettyacademy.com/angularpractice/');
-    cy.get('input[name="name"]:nth-child(2)').type(test_data.name);
-    cy.get('select').select(test_data.gender);
+    homePage.getEditBox().type(test_data.name);
+    homePage.getGender().select(test_data.gender);
 
     // Verifies if the name typed is showed in the field
-    cy.get(':nth-child(4) > .ng-untouched').should(
-      'have.value',
-      test_data.name
-    );
+    homePage.getTwoWayDataBinding().should('have.value', test_data.name);
 
     // Verifies min length
-    cy.get('input[name="name"]:nth-child(2)').should(
-      'have.attr',
-      'minlength',
-      '2'
-    );
+    homePage.getEditBox().should('have.attr', 'minlength', '2');
 
     // Verifies if inline radio is disabled
-    cy.get('#inlineRadio3').should('be.disabled');
+    homePage.getEntrepreneur().should('be.disabled');
+
+    // Pauses running for debug
+    // cy.pause();
+
+    Cypress.config('defaultCommandTimeout', 8000);
 
     // Clicks on the shop link
-    cy.get(':nth-child(2) > .nav-link').click();
+    homePage.getShopTab().click();
 
-    // Checks the card titles and add the one with Blackberry to the cart using the cypress command that is on support commands
-    cy.selectProduct('Blackberry');
+    // Checks the card titles and add to the cart using the cypress command that is on support commands
+    test_data.productName.forEach((element) => {
+      productsPage.selectProduct(element);
+    });
+    productsPage.checkoutButton().click();
+
+    cy.contains('Checkout').click();
+    cy.get('#country').type('India');
+
+    cy.get('.suggestions > ul > li > a').click();
+    cy.get('.checkbox').click();
+    cy.get('input[type="submit"]').click();
+
+    cy.get('.alert').then((element) => {
+      const current_text = element.text();
+
+      expect(current_text.includes('Success')).to.be.true;
+    });
   });
 });
